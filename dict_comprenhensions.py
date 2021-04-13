0eac7d4abcbc99483fec1a038ffd2643aa726f9e
Mauro-CVO <mauo.cortes2@gmail.com>
2021-04-13 15:12:45 -0500

    reto

--- dict_comprenhensions.py
+++ dict_comprenhensions.py
@@ -21,9 +21,12 @@ def run():
     #print(my_dict)
 
     #Dictionary Comprenhension
-    dic = {i: i**3 for i in range(1,101) if i % 3 != 0}
-    print(dic)
+    #dic = {i: i**3 for i in range(1,101) if i % 3 != 0}
+    #print(dic)
 
+    #RETO
+    squares_roots = {i: i**(1/2) for i in range (1,1001)}
+    print(squares_roots)
 
 if __name__ == '__main__':
     run()
