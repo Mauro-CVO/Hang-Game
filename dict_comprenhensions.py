bb00f52c646061538817f75eced2d012b7bc2ac9
Mauro-CVO <mauo.cortes2@gmail.com>
2021-04-13 14:59:07 -0500

    dict comprenhension, mi version

---
+++ dict_comprenhensions.py
@@ -0,0 +1,14 @@
+def run():
+
+    ### numeros de 1 a n, en un dict, donde k = n y v = n**2, mi version
+    #ran = int(input('range: '))
+    #lst = range(ran)
+    #nat_nums = dict(zip([i for i in lst[1:]], [j**3 for j in lst[1:] ]))
+    #for v,k in nat_nums.items():
+    #    print(v, '^2:', k)
+
+
+
+
+if __name__ == '__main__':
+    run()
