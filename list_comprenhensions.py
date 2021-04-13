502ab96ed27edc1117e93bfd2d279e2822f06934
Mauro-CVO <mauo.cortes2@gmail.com>
2021-04-13 14:47:59 -0500

    list comprenhensions, ver 2

--- list_comprenhensions.py
+++ list_comprenhensions.py
@@ -1,9 +1,15 @@
 def run():
-    squares = []
+    '''squares = []
     for i in range(1, 101):
         if i%3 != 0:
             squares.append(i**2)
-    print(squares)
+    print(squares)'''
+
+    '''squares = [i **2 for i in range(1, 101) if  i%3 != 0]
+    print(squares)'''
+
+    lst = [i for i in range(1, 1000) if i%4 == 0  and i%6 == 0 and i%9 == 0]
+    print(lst)
 
 if __name__ == '__main__':
     run()
