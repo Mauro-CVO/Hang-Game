8e657a6994485733256185189e80e3f38e0c0ad0
Mauro-CVO <mauo.cortes2@gmail.com>
2021-04-13 22:14:39 -0500

    Clase usario y elegir palabra al azar

--- Ahorcado/juego_del_ahorcado.py
+++ Ahorcado/juego_del_ahorcado.py
@@ -1,6 +1,64 @@
-class Usuario:
-    def __init__(self):
-        self.vidas = 3
+import random
+import os
 
-    def lifes (self):
-        print(vidas)
+class Usuario:
+    def __init__(self, name):   
+        self.name = name
+
+    def user_name (self):
+        lst = []
+        lst.append(self.name)
+        count = 0
+        #print('lst',lst)
+        with open("./usuarios.txt", 'r+', encoding="utf-8") as f:
+            for line[:5] in f:
+                #print(line)
+                if line.strip() == self.name:
+                    count = 1
+            if count == 1:
+                print('already', self.name)
+            else:
+                #f.write("\n")
+                f.write(self.name)
+                f.write("\n")
+                print('new name', self.name)
+
+    def loose_lifes (self):
+        vida = vida - 1
+    
+    def gain_lifes(self):
+        vida = vida + 1
+
+    def score(self):
+        pass
+
+
+
+def choose_word(lst):
+        num = random.randint(0, len(lst))
+        choosed = lst[num]
+        print(choosed)
+
+
+def random_words():
+    word_lst = []
+    with open("./data.txt", 'r', encoding="utf-8") as f:
+        for word in f:
+            word_lst.append(word.replace("\n", ""))   
+    return(word_lst)
+
+
+
+def run():
+    os.system('clear')
+    name = input('Enter your name: ').upper
+    assert len(nm) < 7, "Name can't be more than 6 character length"
+    
+    user = Usuario(name)
+    user.user_name()
+
+    x = random_words ()
+    w = choose_word(x)
+    
+
+run()
