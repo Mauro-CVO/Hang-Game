0be8351068867c6a1ee472286b97fd30036acd90
Mauro-CVO <mauo.cortes2@gmail.com>
2021-04-14 15:02:20 -0500

    juego funcionando

--- Ahorcado/juego_del_ahorcado.py
+++ Ahorcado/juego_del_ahorcado.py
@@ -15,13 +15,13 @@ class Usuario:
                 if line.strip() == self.name:
                     count = 1
             if count == 1:
-                print('already', self.name)
+                print('Hola', self.name)
             else :
                 f.write(self.name)
                 f.write("\n")
-                print('new name', self.name)
+                print('Bienvenido', self.name)
 
-    def loose_lifes (self, n):
+    '''def loose_lifes (self, n):
         self.lifes = n
         print(self.lifes)
 
@@ -34,8 +34,18 @@ class Usuario:
         
 
     def score(self):
-        pass
+        pass'''
 
+
+def clean():
+    os.system('clear')
+
+def to_str(lst):
+    word = ""
+    for i in lst:
+        word += i
+        word +=" "
+    return word
 
 
 def choose_word(lst):
@@ -45,52 +55,106 @@ def choose_word(lst):
         return(choosed)
 
 
-def random_words():
+def order_words():
     word_lst = []
     with open("./data.txt", 'r', encoding="utf-8") as f:
         for word in f:
             word_lst.append(word.replace("\n", ""))   
     return(word_lst)
 
-def juego(lifes ,word_choosed, count):
-    n = lifes
-    while n > 0:
-        os.system('clear')
-        print(word_choosed)
-        print("Nivel", count,'                       ', 'vidas:', n )
-        sym = "_ " * len(word_choosed)
-        print(sym)
-        if input("respuesta") in  word_choosed:
-            os.system('clear')
-            n = n - 1
-        else:
-            print('you win')
-            count = count + 1
-            break
-    if n == 0:
-        print('you loose')
 
+def stadandar(word):
+
+    repleaced_word = word.maketrans("áéíóú", "aeiou")
+    translated_word = word.translate(repleaced_word).upper()
+    return(translated_word)
+
+def to_list(word):
+    word_2_list = []      
+    word_2_list[:0] = word
+    return word_2_list
+
+def find_letter(inp_letter,word_2_list):
+    order_letter = []
+    for i in range(len(word_2_list)):
+        if inp_letter == word_2_list[i]:
+            order_letter.append(i)
     
-    return(count, n)
+    return order_letter
+
+def dashes(dashes_n,order_letter,word_2_list):
+
+    for i in range(len(word_2_list)):
+        for j in range(len(order_letter)):
+            if i == order_letter[j]:
+                dashes_n[i]=word_2_list[i]
+
+    return(dashes_n)
+
+
+def play(score, word_choosed, count):
+    dashes_n = to_list("_" *len(word_choosed))
+    word = stadandar(word_choosed)
+    word_lst = to_list(word)
+
+
+    while dashes_n != word_lst:
+       
+        clean()
+        print('Nivel', count, '                      ',
+        "Puntuación: ", score)
+        print("\n")
+        print(to_str(dashes_n))
+        print("\n")
+        letter = input("Inserte una letra: ").upper()
+        print("\n")
+        print("\n")
+        print('Puedes escribir exit para terminar la partida.')
+        assert letter != "EXIT", "Hasta la proxima"
+        '''if letter == "EXIT":
+            clean()
+            print("Hasta la proxima")
+            break'''
+        letter_finded = find_letter(letter, word_lst)
+        lifes = letter_finded
+        dashes_n = dashes(dashes_n, letter_finded, word_lst)
+        
+    clean()
+    print("\n")print("\n")print("\n")
+    print(to_str(dashes_n), "Buen Trabajo!!!!!")
+    print("\n")
+    print("\n")
+    input("Presiona Enter para continuar")
+
+    count = count + 1
+    score = score + len(word)
+    return count, score
 
 
 def run():
+
     os.system('clear')
-    nm = input('Enter your name: ')
+    nm = input('¿Cual es tu nombre?: ')
     assert len(nm) < 7, "Name can't be more than 6 character length"
+    clean()
+    print("\n")
+    print("Bienvenido", nm )
+    print("\n")
+    print("\n")
+    input("Presiona Enter para iniciar")
     user = Usuario(nm.upper(), 3)
     user.user_name()
-    print(user.lifes)
-    word_lst_rdm = random_words ()
-    word_choosed = choose_word(random_words())
-    print(word_choosed)
-
-
-    while user.lifes > 0:
-        user.loose_lifes(juego(user.lifes, choose_word(random_words()), 1)[1])
+    orded = order_words()
+    count = [1, 1]
     
-
+    for i in range(len(orded)):
+        count = play(
+            int(count[1]), 
+            choose_word(order_words()), 
+            int(count[0])
+            )
+        
     
+if __name__ == '__main__':
 
-
-run()
+    run()
